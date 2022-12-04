1224d2995e7ea22fe190068325e6144fb1295902
MartinSUEN <112677090+MartinSUEN@users.noreply.github.com>
2022-12-04 13:21:01 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+STUDENT_ID = "00000000000"
+API_KEY = ""
+
+API_ENDPOINT = 'https://api-pytest-runner-test-2223.azure-api.net'
+LOCATIONS = "asia-east2"
+PROJECT_ID = "ite3101-assignment"
---
+++ lab/lab01/t04_nlp.py
@@ -0,0 +1,9 @@
+from google.cloud import language
+
+
+def get_text_sentiment(text):
+    pass
+
+
+def get_text_entities(text):
+    pass
